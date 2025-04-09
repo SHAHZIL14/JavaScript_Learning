16c3a8ad30845dcd945cec98051c02f542901a7e
Mohd Shazil Raza <shazilraza8@gmail.com>
2025-04-09 11:30:38 +0000

    Variables&Scopes

---
+++ 02_VariablesAndScopes.js
@@ -0,0 +1,13 @@
+const accountId="12212";
+let accountName="Mohd Shazil Raza";
+var accountType = "savings";
+accountMember = "premium";
+
+console.table([accountId,accountName,accountType,accountMember]);
+
+// accountId="12222";
+accountName="Mohd sultan Raza";
+accountType = "current";
+accountMember = "normal";
+
+console.table([accountId,accountName,accountType,accountMember]);
