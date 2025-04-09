08174c92b90f48add7cdaac9ca7c4adba98988ac
Mohd Shazil Raza <shazilraza8@gmail.com>
2025-04-09 12:35:12 +0000

    DataType_Conversion

---
+++ ConversionOfDataTypes.js
@@ -0,0 +1,14 @@
+let num = 123;
+let numToStr = String(num);
+
+let str = "22";
+let strToNum =  Number(str);
+
+let bool = true;
+let boolToNum = Number(bool);
+
+let num1 = 1;
+let numToBool =Boolean(num1);
+
+console.table([numToStr,numToBool,strToNum,boolToNum]);
+
