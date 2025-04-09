ddf370188b107471faecbd5297e8bc7c22078c56
Mohd Shazil Raza <shazilraza8@gmail.com>
2025-04-09 11:52:36 +0000

    DataTypes

---
+++ 03_DataTypes.js
@@ -0,0 +1,8 @@
+let ex1 = 23;
+let ex2 = "Hii";
+let ex3 = true;
+let ex4 = null;
+let ex5 = Symbol("description here");
+let ex6 =  BigInt( 12000000000000000);
+let ex7 = undefined;
+console.table([typeof(ex1),typeof(ex2),typeof(ex3),typeof(ex4),typeof(ex5),typeof(ex6),typeof(ex7)]);
