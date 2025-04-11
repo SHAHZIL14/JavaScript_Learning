077f919baca494c9d9861dd4622ca24bc5e659af
Mohd Shazil Raza <shazilraza8@gmail.com>
2025-04-11 10:56:16 +0000

    StringMethods

---
+++ 09_StringMethods.js
@@ -0,0 +1,42 @@
+/* There are ways to implement string in JS.
+1. As a variable normal:-
+let str = "This is first way to implement it";
+
+2. As an object:-
+let newStr = new string("This is the second way to implement it");
+*/
+
+let str = "This is first way to implement it \n";
+const newString =  new String('Shahzil');
+console.log(newString.charAt(3));
+
+/* String Methods:-
+String length
+String charAt()
+String charCodeAt()
+String at()
+String [ ]
+String slice()
+String substring()
+String substr()
+String toUpperCase()
+String toLowerCase()
+String concat()
+String trim()
+String trimStart()
+String trimEnd()
+String padStart()
+String padEnd()
+String repeat()
+String replace()
+String replaceAll()
+String split()
+String match()
+String matchAll()
+String includes()
+String startsWith()
+String endsWith()
+String indexOf()
+String lastIndexOf()
+String search()
+*/
