6580954d162596d5e2cdf33b80a65860c4dca182
Mohd Shazil Raza <shazilraza8@gmail.com>
2025-04-11 09:44:51 +0000

    Memory Allocations

---
+++ 08_MemoyAllocations.js
@@ -0,0 +1,12 @@
+// There are two ways in which javascript allocates memory for the program to be executed.
+
+/* STACK MEMORY
+1. The javascript compiler allocates the memory in the form of stack for primitve Data types.
+2. The locations of memory shared within the program with the copy of value.
+3. Examples:- String,Number,Boolean,BigInt and more.
+
+HEAP MEMORY
+1. The javascript compiler allocates the memory in the form of stack for Non-primitve Data types.
+2. In this method of allocation , the variables and functions get passed with the pointer address of that in memory.
+3. Examples:- objects , Arrays, Funcions.
+*/
