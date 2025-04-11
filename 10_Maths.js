88497dec2cd1f83dc0953c2b3f790d83b35da539
Mohd Shazil Raza <shazilraza8@gmail.com>
2025-04-11 12:02:13 +0000

    Math Object in JS

---
+++ 10_Maths.js
@@ -0,0 +1,27 @@
+/* There is a 'Math' object in javascript which includes all the important and frequently used functions in javascript.
+
+Some of the MATH functions are mentioned below:-
+Math.E        // returns Euler's number
+Math.PI       // returns PI
+Math.SQRT2    // returns the square root of 2
+Math.SQRT1_2  // returns the square root of 1/2
+Math.LN2      // returns the natural logarithm of 2
+Math.LN10     // returns the natural logarithm of 10
+Math.LOG2E    // returns base 2 logarithm of E
+Math.LOG10E   // returns base 10 logarithm of E
+
+
+Math.round(x)	Returns x rounded to its nearest integer
+Math.ceil(x)	Returns x rounded up to its nearest integer
+Math.floor(x)	Returns x rounded down to its nearest integer
+Math.trunc(x)	Returns the integer part of x (new in ES6)
+Math.sign(x) returns if x is negative, null or positive:
+Math.pow(x,y) returns the exponentiation of x t the power y
+Math.sqrt(x) returns the square root of x
+Math.abs(x) returns the posstived signed value of x
+Math.min() and Math.max() returns the maximum and minimum value among the given arguments
+Math.random() returns the random value between 0-1
+Math.log(x) returns the natural logarithm of x
+Math.log2(x) returns the base 2 logarithm of x
+Math.log10(x) returns the base 10 logarithm of x
+*/
