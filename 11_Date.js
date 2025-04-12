973886dcb8d577e0b31e0550b14ad8831daf35e4
Mohd Shazil Raza <shazilraza8@gmail.com>
2025-04-12 07:44:23 +0000

    Date&Time

---
+++ 11_Date.js
@@ -0,0 +1,4 @@
+// let d = new Date();
+// console.table([d.toDateString(),d.toLocaleString(),d.toLocaleDateString(),d.toLocaleTimeString(),d.toJSON(),d.getMinutes()]);
+let createdDate =  new Date(2025,3,12);
+console.log(createdDate.toDateString());
