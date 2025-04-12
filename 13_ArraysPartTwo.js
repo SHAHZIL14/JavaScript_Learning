97ddae6cbb1d0eb4890d9b085def534c964c38d3
Mohd Shazil Raza <shazilraza8@gmail.com>
2025-04-12 08:39:25 +0000

    ArrayTwo

---
+++ 13_ArraysPartTwo.js
@@ -0,0 +1,7 @@
+/* Another methods of array :-
+There is an object Array in javascript and many methods actually lies within this object.
+1. Array.is(x) : returns the boolean value that the x is array or not.
+2. Array.from(x); returns new array from the element or list of elements x;
+3. Array.of(x) : Also returns new array from the element x or list of element x.
+4. arr.flat(levelOfDepth): Flat the whole array if any element of the array is also an array and further.
+*/
