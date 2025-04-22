0216d70c11b7696d0948e83ad68c1a8ce4077e56
Mohd Shazil Raza <shazilraza8@gmail.com>
2025-04-22 14:58:04 +0000

    Intro to Iterations in JS

---
+++ 22_IterationIntro.js
@@ -0,0 +1,13 @@
+/* In any  of the programming languages. There are many cases where we 
+have to write the same function again and again but with some modifications which is very limited throughout 
+the function. These are done with loops or iterations.*/
+//First we have loop as FOR//
+const arr = [1,2,3,4,5,6,7,8,9,10];
+for (let index = 0; index < arr.length; index++) {
+    const element = arr[index];
+    console.log(element);
+}
+
+/*Some important keywords in for loops
+1. break-It is used to break the control in loop and exit it .
+2. continue-It is use to skip the particular iteration in which the keyword used in.*/
