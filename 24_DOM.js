1eba54207e9e80b408adf8425aba66ed72a3d66d
Mohd Shazil Raza <shazilraza8@gmail.com>
2025-05-09 18:51:31 +0000

    Added intro to DOM

---
+++ 24_DOM.js
@@ -0,0 +1,20 @@
+/*In HTML we have browser oriented object as WINDOW object inside which we have several 
+Browser objects as The tree of html elements which we call as DOCUMENT OBJECT MODEL as DOM.
+The general tree structure of DOM is :
+
+WINDOW OBJECT:-
+
+1.HEAD TAG:
+
+ 1.META
+ 2.LINKS
+ 3.TITLE
+ 4.OTHER SEO RELATED
+
+2.BODY TAG:
+ 1.Programmer used TAGs
+*/
+
+/* We access and manipulate all the objects and elements related to DOM with some document 
+object functions and methods*/
+
